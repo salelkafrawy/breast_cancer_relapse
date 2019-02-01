631335a31a359191d599660853e23155e3f8d1c9
Sara Ebrahim <selkafrawy@aimsammi.org>
2019-02-01 13:34:06 +0200

    final version

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +14,23 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.6805555555555556
+# Average CV score on the training set was:0.951388888888889
 exported_pipeline = make_pipeline(
-    Normalizer(norm="max"),
-    DecisionTreeClassifier(criterion="gini", max_depth=6, min_samples_leaf=4, min_samples_split=4)
+    make_union(
+        FunctionTransformer(copy),
+        make_union(
+            FunctionTransformer(copy),
+            make_union(
+                FunctionTransformer(copy),
+                make_union(
+                    FunctionTransformer(copy),
+                    FunctionTransformer(copy)
+                )
+            )
+        )
+    ),
+    StandardScaler(),
+    LinearSVC(C=25.0, dual=False, loss="squared_hinge", penalty="l2", tol=1e-05)
 )
 
 exported_pipeline.fit(training_features, training_target)
